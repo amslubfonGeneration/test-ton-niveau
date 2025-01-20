9e1b511356d502fa5f0f836e7a79166c3ffa24f2
AMOUSSOU <amslubfonGeneration>
2025-01-20 03:14:45 +0100

    Version 4

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { rootDir } from "./conf.js"
 import { c, latex, python, scilab } from "./action.js"
 
 const corsConfig = {
-    origin: "*",
+    origin:['http://localhost:5173'],
     Credential: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }
