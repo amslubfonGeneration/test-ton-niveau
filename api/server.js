cd351c4c9c4dcea38d57f672eaa9448c787d5ce8
AMOUSSOU <amslubfonGeneration>
2025-01-21 15:50:38 +0100

    Version new

--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
+import fastify from "fastify";
+import fastifyfunction from "../src/index.js";
+const app = fastify()
 
-      
+export default async function handler(req, res){
+   fastifyfunction(app)
+}
+
+
+
