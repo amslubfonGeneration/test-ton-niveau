b442bec55a25bff796ff8d02594225725660714a
AMOUSSOU <amslubfonGeneration>
2025-01-21 18:12:14 +0100

    Version 2

--- api/server.js
+++ api/server.js
@@ -1,10 +1,62 @@
-import fastify from "fastify";
-import fastifyfunction from "../src/index.js";
+import fastify  from "fastify"
+import fastifyFormbody from "@fastify/formbody"
+import fastifyStatic from "@fastify/static"
+import fastifyView from "@fastify/view"
+import ejs from 'ejs'
+import { join } from "node:path"
+import cors from "cors"
+import { rootDir } from "./conf.js"
+import { c, latex, python, scilab } from "./action.js"
+
+
 const app = fastify()
+app.register(fastifyView,{
+    engine: {
+        ejs
+    }
+})
+app.register(fastifyStatic, {
+    root:join(rootDir,'public')
+})
+app.register(fastifyFormbody)
 
-export default async function handler(req, res){
-   fastifyfunction(app)
+
+export default async function fastifyfunction(app) {
+    app.get("/",(req,res)=>{
+        return res.redirect('index.html')
+    })
+    app.get('/python/test', async (req,res) =>{
+        return res.view('template/form.ejs',{
+            python:"python"
+        })
+    })
+    app.get('/latex/test', async (req,res) =>{
+        return res.view('template/form.ejs',{
+            latex:"latex"
+        })
+    })
+    app.get('/scilab/test', async (req,res) =>{
+        return res.view('template/form.ejs',{
+            scilab:"scilab"
+        })
+    })
+    app.get('/c++/test', async (req,res) =>{
+        return res.view('template/form.ejs',{
+            c:"c++"
+        })
+    })
+
+    app.post('/python/test', python)
+    app.post('/c++/test', c)
+    app.post('/scilab/test', scilab)
+    app.post('/latex/test', latex)
+
+
+    app.setErrorHandler((error,req,res) => {
+        console.error(error)
+        res.statusCode = 500
+        return {
+            error: error.message  
+        }
+    })
 }
-
-
-
