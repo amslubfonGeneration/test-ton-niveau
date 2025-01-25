191cf0f5ae26a45e79b8293e968b0b53220b76d6
AMOUSSOU <amslubfonGeneration>
2025-01-25 10:09:30 +0100

    Version new

--- src/conf.js
+++ src/conf.js
@@ -6,5 +6,5 @@ export const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
 
 
 
-const supabase_url = process.env.SUPABASE_URL
-const supabase_key = process.env.SUPABASE_KEY
+export const supabase_url = process.env.SUPABASE_URL
+export const supabase_key = process.env.SUPABASE_KEY
