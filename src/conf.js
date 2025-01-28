c9d849f70848534c1d0f8ea59b3b34658009a86c
AMOUSSOU <amslubfonGeneration>
2025-01-28 21:14:32 +0100

    new version

--- src/conf.js
+++ src/conf.js
@@ -1,13 +1,3 @@
-import dotenv from 'dotenv'
-import {fileURLToPath} from 'node:url'
-import {dirname, join} from 'node:path'
-
-export const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
-dotenv.config({
-    path: join(rootDir, 'fichier.env'),
-    debug: true,
-    encoding: 'utf8'
-})
 
 
 export const supabase_url = process.env.SUPABASE_URL
